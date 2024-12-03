7d123be9b314e54ae812cd4a50728bca7ceefde7
kervin <101122530+userkace@users.noreply.github.com>
2024-12-03 21:38:21 +0800

    4.9.5

--- themes/list/blue.ts
+++ themes/list/blue.ts
@@ -14,7 +14,6 @@ const tokens = {
     c900: "#03102a"
   },
   shade: {
-    c25: "#5d7db3",
     c50: "#756790",
     c100: "#60527a",
     c200: "#4a3f60",
@@ -122,7 +121,7 @@ export default createTheme({
 
       mediaCard: {
         hoverBackground: tokens.shade.c600,
-        hoverAccent: tokens.shade.c25,
+        hoverAccent: tokens.shade.c50,
         hoverShadow: tokens.shade.c900,
         shadow: tokens.shade.c700,
         barColor: tokens.ash.c200,
